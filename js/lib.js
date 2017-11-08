13749afc64ec9f3bde6bceec88388d3b364930a7
rafaelrozendo <rafaelrozendo@bancobbm.com.br>
2017-11-08 19:13:32 -0200

    autenticando pelo header

--- js/lib.js
+++ js/lib.js
@@ -112,9 +112,13 @@ function trySampleRequest() {
     var params = getAuthenticationParameters();
     if (params && params['access_token']) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET',
+      /*xhr.open('GET',
           'https://www.googleapis.com/drive/v3/about?fields=user&' +
-          'access_token=' + params['access_token']);
+          'access_token=' + params['access_token']);*/
+	  xhr.setRequestHeader('Authorization', "Bearer " + params['access_token']);
+	  xhr.open('GET',
+          'https://www.googleapis.com/drive/v3/about?fields=user';
+		  
       xhr.onreadystatechange = function (e) {
         console.log(xhr.response);
       };
